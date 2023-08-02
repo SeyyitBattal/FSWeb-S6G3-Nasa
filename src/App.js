a1b76d31bb4183b7afb26510b09e1d8e6b889351
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-08-02 16:52:54 +0300

    Basic HTML design is done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  ////////////////
+
   return (
     <div className="App">
-      <p>
-        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
-		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <div className="myHeader">Header Bölümü</div>
+      <hr />
+      <div className="mainArea">
+        <div className="myLeftSide">Sol taraftaki Side Bar</div>
+        <div className="myRightSide">
+          ANA BÖLÜM
+          <p>
+            NASA uygulamasını yapmak için README.md dosyasıdaki talimatları
+            takip edin İyi eğlenceler!{" "}
+            <span role="img" aria-label="go!">
+              🚀
+            </span>
+            !
+          </p>
+        </div>
+      </div>
+      <hr />
+      <div className="myFooter">Footer Bölümü</div>
     </div>
   );
 }
