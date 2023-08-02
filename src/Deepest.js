bada38e4e84ee11deee2e6091b0728e172a8d2fc
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-08-02 19:26:29 +0300

    Static data is done

--- src/Deepest.js
+++ src/Deepest.js
@@ -1,6 +1,19 @@
 const Deepest = (props) => {
   const { nasaDegeri } = props;
   console.log("Deepest'tan gelen: ", nasaDegeri);
-  return <div></div>;
+
+  return (
+    <div className="deepestContainer">
+      <div>Ait olduğu kişi: {nasaDegeri.copyright}</div>
+      <div>Tarih: {nasaDegeri.date}</div>
+      <div>Açıklama: {nasaDegeri.explanation}</div>
+      <div>HD Hali: {nasaDegeri.hdurl}</div>
+      <div>Başlık: {nasaDegeri.title}</div>
+      <div>URL: {nasaDegeri.url}</div>
+      <div></div>
+
+      <div></div>
+    </div>
+  );
 };
 export default Deepest;
